0d3131cf713880741e86a8c3b51342139d8b076b
VLADIMIR GUSSEV <zer0.n00ble@gmail.com>
2026-05-13 09:05:44 +0500

    изменен порт и интерфейс для запуска на 0.0.0.0:5070

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ atexit.register(lambda: scheduler.shutdown())
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5070, debug=True)
